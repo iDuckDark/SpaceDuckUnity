a44ad08f3ca56f179e85f63065b19d19f727f1d2
iDarkDuck <nevinganesan@hotmail.com>
2019-03-17 13:13:50 -0400

    asteriod explosion and player explosion

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+
+    public GameObject explosion, playerExplosion;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Boundary")
         {
             return;
         }
+
+        //this is to make explosion effects
+        GameObject clone = Instantiate(explosion, transform.position, transform.rotation);
+
+        if(other.tag == "Player") {
+            GameObject pClone = Instantiate(playerExplosion, other.transform.position, other.transform.rotation);       
+        }
+
         //order doesnt matter because by the end of the frame
         Destroy(other.gameObject);
         Destroy(gameObject); //the asteriod and all its children
