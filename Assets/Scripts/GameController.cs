2933ca305a2e8906e9adea1d2cb1cc1e3d2b855b
iDarkDuck <nevinganesan@hotmail.com>
2019-03-17 14:33:15 -0400

    making asteriods

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,18 +6,25 @@ public class GameController : MonoBehaviour
 {
     public GameObject hazard;
     public Vector3 spawnValues;
+    public int hazardCount;
+    public float startWait, spawnWait;
 
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(startWait);
+        for (int i = 0; i < hazardCount; i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(hazard, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnWaves();
+        StartCoroutine(SpawnWaves());
     }
 
     // Update is called once per frame
