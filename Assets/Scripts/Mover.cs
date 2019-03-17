521ae6b2dd7e62a7ffc00f001e4ad86c07da0f25
iDarkDuck <nevinganesan@hotmail.com>
2019-03-16 23:38:38 -0400

    bolt shoots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * speed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
