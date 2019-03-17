43728f33eccc91a3518b3995a1dd9178218c7801
iDarkDuck <nevinganesan@hotmail.com>
2019-03-17 00:11:50 -0400

    destructor for shooting

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
