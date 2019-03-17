fcc1aa2a615a666b7199a527280a71b7de9a7621
iDarkDuck <nevinganesan@hotmail.com>
2019-03-17 00:29:23 -0400

    .

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.angularVelocity
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
