7853a95b57b735fd871e6d13031dd1fcacf9ec37
iDarkDuck <nevinganesan@hotmail.com>
2019-03-17 00:46:25 -0400

    fix object asteroid destroy by bolt

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Boundary")
+        {
+            return;
+        }
+        //order doesnt matter because by the end of the frame
+        Destroy(other.gameObject);
+        Destroy(gameObject); //the asteriod and all its children
+    }
     // Start is called before the first frame update
     void Start()
     {
